532bb21533997f454f95be1e8a9e08431d154066
Restu Muzakir <hi@restuhaqza.dev>
2024-02-24 00:08:08 +0700

    fix: eslint fix

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -11,14 +11,31 @@ import {onRequest} from "firebase-functions/v2/https";
 import * as express from "express";
 import {Request, Response} from "express";
 import MailingHandler from "./handlers/mailingList";
+import * as cors from "cors";
 
 const app = express();
 
 const mailingListHandler = new MailingHandler();
 
+// cors policy
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    const allowedOrigin = ["https://restuhaqza.dev"];
+    if (allowedOrigin.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req: Request, res: Response) => {
   return res.status(200).json({message: "Hello from Restu Universe!"});
 });
-app.post("/newsletter/subscribe", mailingListHandler.subscribe.bind(mailingListHandler));
+app.post("/newsletter/subscribe", mailingListHandler.subscribe);
 
 export const api = onRequest(app);
